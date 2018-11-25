fbfa4aa2fbed56d97de52a2ac4c3c6a67b3d6281
KonnariOy <sami.salonpaa@student.oulu.fi>
2018-11-25 19:36:59 +0200

    Movement Paint
    Game Paints over tiles that can be moved to.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,6 +10,7 @@ public class GameManager : MonoBehaviour {
         public int owner;
         public int bees;
         public GameObject beeObject;
+		public GameObject paintTile;
 
         public Tile(bool tree, int own, int bee)
         {
@@ -19,19 +20,43 @@ public class GameManager : MonoBehaviour {
         }
     }
 
+	/* Game Values */
+	
+	public int turnCount = 0;
+	
     public int columns = 20;
     public int rows = 20;
     public static GameManager instance = null;              //Static instance of GameManager which allows it to be accessed by any other script.
     private StartGame startScript;
+	
+	/* Game Field Holder */
     private Transform mapHolder;
+	
+	/* GameObjects, prefabricated */
     public GameObject GrassTile;
     public GameObject TreeTile;
     public GameObject BeeTile;
-    int[,] treePos = { { 0, 3 }, { 11, 10 }, { 19, 14 } };
-    int[,] beePos = { { 10, 10 }, {12, 12} , { 19, 14 } };
-    int[] beeOwners = { 0, 0, 1 };
-    private bool myTurn = false;
-    public GameObject selectedArmy = null;
+	public GameObject MoveToBeeTile;
+	public GameObject MoveToTreeTile;
+	public GameObject MoveToTile;
+	// Army Script object
+	public GameObject selectedArmy = null;
+	
+	/* Coordinates */
+    int[,] treePos = { { 0, 3 }, { 11, 10 }, { 19, 14 }, { 5, 8 } };
+    int[,] beePos = { { 10, 10 }, {12, 12} , { 19, 14 }, { 6, 8 } };
+    int[] beeOwners = { 0, 0, 1, 3 };
+	int[] treeOwners = { 0, 0, 1 };
+	
+	int[,] tempLayoutPos = {{1,1},{0,0}};
+    
+	/* Move Paint Logic Variables */
+	int paintX;
+	int paintY;
+	public bool paintOn = false;
+	
+	/* Game Logic */
+	private bool myTurn = false;
     public float tileSize = 0.32f;
     private Vector3 armyDestination;
     public bool selectDestination = false;
@@ -81,6 +106,9 @@ public class GameManager : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
+		
+		bool moveToDestination = false;
+		
         if (!myTurn & currentIndex == myIndex)
         {
             Debug.Log("Your turn.");
@@ -94,24 +122,40 @@ public class GameManager : MonoBehaviour {
             currentIndex++;
             currentIndex %= playerCount;
         }
+		
+		if (Input.GetButtonDown("Fire1") & myTurn & !paintOn) {
+			armyDestination = Camera.main.ScreenPointToRay(Input.mousePosition).origin;
+            paintX = (int)System.Math.Round(selectedArmy.transform.position.x / tileSize, 0);
+            paintY = (int)System.Math.Round(selectedArmy.transform.position.y / tileSize, 0);
+			paintMovePresentTile(paintX, paintY, true);
+			paintOn = true;
+		}
+	
         if (Input.GetButtonDown("Fire2") & myTurn & selectDestination)
         {
             armyDestination = Camera.main.ScreenPointToRay(Input.mousePosition).origin;
+			
             Debug.Log(armyDestination);
             int destX = (int)System.Math.Round(armyDestination.x / tileSize, 0);
             int destY = (int)System.Math.Round(armyDestination.y / tileSize, 0);
             int locX = (int)System.Math.Round(selectedArmy.transform.position.x / tileSize, 0);
             int locY = (int)System.Math.Round(selectedArmy.transform.position.y / tileSize, 0);
-
+			
             if (System.Math.Abs(destX - locX) > maxMoveDistance | System.Math.Abs(destY - locY) > maxMoveDistance |
                 destX < 0 | destY < 0 | destX > (columns - 1) | destY > (rows - 1) | (destX == locX & destY == locY))
             {
                 Debug.Log("Invalid move.");
+				if(paintOn)
+					;
+				else
+					paintMovePresentTile(paintX, paintY, false);
+				
             }
             else
             {
+				
                 tiles[locX][locY].bees = 0;
-                bool moveToDestination = true;
+                moveToDestination = true;
                 if (tiles[destX][destY].bees > 0)
                 {
                     if (tiles[destX][destY].owner == myIndex)
@@ -128,13 +172,13 @@ public class GameManager : MonoBehaviour {
                         int result = selectedArmy.GetComponent<BeeScript>().count - tiles[destX][destY].bees;
                         if (result > 0)
                         {
-                            Debug.Log("Your army won.");
+                            Debug.Log("Attacker won.");
                             Destroy(tiles[destX][destY].beeObject);
                             selectedArmy.GetComponent<BeeScript>().SetCount(result);
                         }
                         else if (result < 0)
                         {
-                            Debug.Log("Other army won.");
+                            Debug.Log("Defender won.");
                             Destroy(selectedArmy);
                             tiles[destX][destY].beeObject.GetComponent<BeeScript>().SetCount(-result);
                             tiles[destX][destY].bees = -result;
@@ -142,7 +186,7 @@ public class GameManager : MonoBehaviour {
                         }
                         else
                         {
-                            Debug.Log("Fight wiped out both armies.");
+                            Debug.Log("Mutual wipe out.");
                             Destroy(tiles[destX][destY].beeObject);
                             Destroy(selectedArmy);
                             tiles[destX][destY].bees = 0;
@@ -165,11 +209,94 @@ public class GameManager : MonoBehaviour {
                     currentIndex++;
                     currentIndex %= playerCount;
                     myTurn = false;
+					
                 }
                 selectDestination = false;
+				paintOn = false;
             }
+			if(paintOn)
+					;
+				else
+					paintMovePresentTile(paintX, paintY, false);
         }
     }
+	
+	bool cTreePosit(int x, int y) {
+		return tiles[x][y].hasTree;
+	}
+	
+	bool cBeePosit(int x, int y) {
+		if(tiles[x][y].bees > 0)
+			return true;
+		else
+			return false;
+	}
+	
+	void paintMovePresentTile(int x, int y, bool maybe) {
+		int a = x + maxMoveDistance + 1;
+		int b = y + maxMoveDistance + 1;
+		int c = x - maxMoveDistance;
+		int d = y - maxMoveDistance;
+		if(a > 20)
+			a = 20;
+		if(b > 20)
+			b = 20;
+		if(c < 0)
+			c = 0;
+		if(d < 0)
+			d = 0;
+		while(c < a) {
+			while(d < b) {
+				if(x == c && y == d)
+					;
+				else {
+					if(maybe)
+						makeTempTile(c,d);
+					else
+						removeTempTile(c,d);
+				}
+				++d;
+			}
+			d = y - maxMoveDistance;
+		++c;
+		}
+	}
+	
+	void makeTempTile(int x, int y) {
+		
+		Debug.Log("MakeTempTile");
+		GameObject instance;
+		GameObject toInstantiateGrass = MoveToTile;
+		GameObject toInstantiateTree = MoveToTreeTile;
+		GameObject toInstantiateBee = MoveToBeeTile;
+		{
+			//Instantiate the GameObject instance using the prefab chosen for toInstantiate at the Vector3 corresponding to current grid position in loop, cast it to GameObject.
+			Debug.Log("MakeTempTile1");
+			if(cTreePosit(x,y)) // test if tree / bee here, apply bee/tree tile based on it.
+			{
+				instance = Instantiate(toInstantiateTree, new Vector3(x * tileSize, y * tileSize, -2f), Quaternion.identity) as GameObject;
+				Debug.Log("MakeTempTile1_1");
+			}
+			else if(cBeePosit(x,y)	) {
+				instance = Instantiate(toInstantiateBee, new Vector3(x * tileSize, y * tileSize, -2f), Quaternion.identity) as GameObject;
+				Debug.Log("MakeTempTile1_2");
+			}
+			else {
+				instance = Instantiate(toInstantiateGrass, new Vector3(x * tileSize, y * tileSize, -2f), Quaternion.identity) as GameObject;	
+				Debug.Log("MakeTempTile1_3");
+			}
+			//Set the parent of our newly instantiated object instance to boardHolder, this is just organizational to avoid cluttering hierarchy.
+			//mapHolder = new GameObject("Overlay").transform;
+			instance.transform.SetParent(mapHolder);
+			tiles[x][y].paintTile = instance;
+		}
+	}
+	
+	void removeTempTile(int x, int y) {
+		//Debug.Log("DestroyTempTile1_3");
+		Destroy(tiles[x][y].paintTile);
+		
+	}
 
     //Creates the map
     void MapSetup()
@@ -243,4 +370,10 @@ public class GameManager : MonoBehaviour {
             }
         }
     }
+	
+	public void turnChange() 
+	{
+			IncreaseBeeCount(1);
+			turnCount = turnCount + 1;
+	}
 }
